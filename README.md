90e7237529659aec87c9efef4ef80cd462d5ed0f
Linh Dinh <nhlinh1803@gmail.com>
2023-02-26 16:50:51 -0800

    Completed Java Assignment 005

--- README.md
+++ README.md
@@ -4,20 +4,44 @@
 As a new Java developer, you will be required to familiarize yourself with existing code before beginning to develop. That's what this section is about, developing your code tracing skills and familiarizing yourself with parts you may be able to use.
 * Scan through all the existing code and note what you have to work with.
     * Method names
+      * convertC2F, convertC2K, convertF2C, convertF2K, convertK2C, getTemp, getUnitChoice, main
     * Variables
+      *  celsius, fahrenheit, kelvin, unit
+  <br>
 * Using Java terminology, break down every piece of the method **convertF2K**.
     * What is the access modifier?
+      * public
     * Is it a class or object method, how do you know?
+      * class method because it is "static"
     * What is its return type?
+      * double
     * What parameters does it require, and what are the parameter(s) datatype(s)?
+      * fahrenheit (type: double)
     * Describe for me how the body of the method executes (i.e. its flow of execution).
+      * The **convertF2K** method converts a temperature value from Fahrenheit to Kelvin. 
+       It does this by first converting the Fahrenheit temperature to Celsius using the **convertF2C** method, 
+       and then converting the Celsius temperature to Kelvin using the **convertC2K** method. 
+       The final result is the Kelvin temperature value.
 * Using Java terminology, break down method **getUnitChoice**.
     * What is the access modifier?
+      * public
     * Is it a class or object method, how do you know?
+      * class method because it is "static"
     * What is its return type?
+      * String
     * What parameters does it require, and what are the parameter(s) datatype(s)?
+      * no parameter
     * Describe for me how the body of the method executes (i.e. its flow of execution).
+      * The getUnitChoice method displays a menu of temperature unit choices, 
+      prompts the user for input, reads the input, and returns it as a String value. 
+      The method uses System.out.printf to format the menu and Scanner.nextLine to read the user's input.
     * How do the format specifiers differ than what you've seen before and what do they do?
+      * %-4s used to format a string with a minimum width of 4 characters, **-** means left alignment.
+       <br> Example: it adds 1 addition space to the right of **C).** because there are only 3 characters.
+      * %40s used to format string with a minimum width of 40 characters, right alignment.
+       <br> Example: it adds 13 additional spaces to the left of **To convert a °C temperature** 
+       because there are only 27 characters.
+
 
 ## Part 2: - Implement Missing Method: convertK2F
 This Part is aimed at teaching you to use methods and code you have to add new functionality to existing code. In this case you will add the missing conversion method, but you don't need to know math :-)
--- src/TempConversion.java
+++ src/TempConversion.java
@@ -1,10 +1,17 @@
+/**
+ * @author Trevor Hartman
+ * @author Linh Dinh
+ *
+ * @since version 1.0
+ */
+
 import java.util.Scanner;
 
 public class TempConversion {
     static final Scanner input = new Scanner(System.in);
 
     public static double convertC2F(double celsius) {
-        return (celsius * (9.0/5)) + 32.0;
+        return (celsius * (9.0 / 5)) + 32.0;
     }
 
     public static double convertC2K(double celsius) {
@@ -23,6 +30,10 @@ public class TempConversion {
         return kelvin - 273.15;
     }
 
+    public static double convertK2F(double kelvin) {
+        return convertC2F(convertK2C(kelvin));
+    }
+
     public static double getTemp(String unit) {
         System.out.printf("%-40s : ", String.format("Please enter °%s temperature to convert", unit));
         return Double.parseDouble(input.nextLine());
@@ -37,8 +48,52 @@ public class TempConversion {
     }
 
     public static void main(String[] args) {
-        while(true) {
+        while (true){
+            //Requests a temperature Unit to convert from
+            System.out.println("Enter temperature unit you want to convert from: ");
+            String from = getUnitChoice();
+            //Quits the program if Q is entered for either choice
+            if (from.equalsIgnoreCase("Q")){
+                System.out.println("Program exited.");
+                break;
+            }
+            //Requests a temperature Unit to convert to
+            System.out.println("Enter temperature unit you want to convert to: ");
+            String to = getUnitChoice();
+            //Quits the program if Q is entered for either choice
+            if (to.equalsIgnoreCase("Q")){
+                System.out.println("Program exited.");
+                break;
+            }
+            //Requests the double temperature value to convert
+            double input = getTemp(from);
+            // Uses a switch or enhanced switch conditional to call the proper conversion method.
+            switch (from.toUpperCase() + to.toUpperCase()){
+                case "CF":
+                    System.out.printf("Result %.6f°%s is %.6f°%s%n",input,from,convertC2F(input),to);
+                    break;
+                case "CK":
+                    System.out.printf("Result %.6f°%s is %.6f°%s%n", input,from,convertC2K(input),to);
+                    break;
+                case "FC":
+                    System.out.printf("Result %.6f°%s is %.6f°%s%n", input,from,convertF2C(input),to);
+                    break;
+                case "FK":
+                    System.out.printf("Result %.6f°%s is %.6f°%s%n", input,from,convertF2K(input),to);
+                    break;
+                case "KC":
+                    System.out.printf("Result %.6f°%s is %.6f°%s%n", input,from,convertK2C(input),to);
+                    break;
+                case "KF":
+                    System.out.printf("Result %.6f°%s is %.6f°%s%n", input,from,convertK2F(input),to);
+                    break;
+                default:
+                    System.out.println("No match");
+
+            }
+
 
         }
+
     }
 }
